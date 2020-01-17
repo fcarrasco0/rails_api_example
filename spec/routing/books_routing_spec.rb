d049abbd319d57d706a1d653c20a4a88ae76b1f2
Carrasco <fernando.carrasco0@gmail.com>
2020-01-17 11:58:09 -0300

    + routing tests ok

--- spec/routing/books_routing_spec.rb
+++ spec/routing/books_routing_spec.rb
@@ -1,30 +1,30 @@
 require "rails_helper"
 
-RSpec.describe BooksController, type: :routing do
+RSpec.describe Api::V1::BooksController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/books").to route_to("books#index")
+      expect(:get => "/api/v1/books").to route_to("api/v1/books#index")
     end
 
     it "routes to #show" do
-      expect(:get => "/books/1").to route_to("books#show", :id => "1")
+      expect(:get => "/api/v1/books/1").to route_to("api/v1/books#show", :id => "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/books").to route_to("books#create")
+      expect(:post => "/api/v1/books").to route_to("api/v1/books#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/books/1").to route_to("books#update", :id => "1")
+      expect(:put => "/api/v1/books/1").to route_to("api/v1/books#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/books/1").to route_to("books#update", :id => "1")
+      expect(:patch => "/api/v1/books/1").to route_to("api/v1/books#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/books/1").to route_to("books#destroy", :id => "1")
+      expect(:delete => "/api/v1/books/1").to route_to("api/v1/books#destroy", :id => "1")
     end
   end
 end
