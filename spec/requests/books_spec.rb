42eb63a8e3fa0f59586b586e30695f569c90bc82
Carrasco <fernando.carrasco0@gmail.com>
2020-01-18 13:15:37 -0300

    # refactoring test requests

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,82 +2,107 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
   describe "GET /books #index" do
-    it "returns a list of books" do
-
+    
+    before do
       FactoryBot.create_list(:book, 10)
-
       get '/api/v1/books'
+    end
+    
+    it "returns 200 if with books array" do
       expect(response).to have_http_status(200)
+    end
 
-      #verifying if json response has all keys
+    it "checks if json_response is an array of book object" do
       expect(json_response[0].keys).to match_array(book_keys)
     end
+
   end
 
   describe "GET /books/1 #show" do
-    it "returns a single book" do
+    
+    before do
       FactoryBot.create(:book)
-
       get '/api/v1/books/1'
-      expect(response).to have_http_status(200)
-
-      #verifying if json response has all keys
-      expect(json_response.keys).to match_array(book_keys)
-
     end
+    
+    it "returns 200 if find book" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "checks if json_response a book object" do
+      expect(json_response.keys).to match_array(book_keys)  
+    end
+    
   end
 
   describe "POST /books #create" do
-    it "returns the created book" do
+    
+    book_params = { 
+      :title => 'test',
+      :author => 'author_test',
+      :publisher => 'fake_publisher',
+      :genre => "Fiction"
+    }
 
-      book_params = { 
-        :title => 'test',
-        :author => 'author_test',
-        :publisher => 'fake_publisher',
-        :genre => "Fiction"
-      }
-      
+    before do
       post '/api/v1/books', params: {book: book_params}
-      
+    end
+    
+    it "returns 201 if created book" do
       expect(response).to have_http_status(201)
-      
+    end
+
+    it "checks if json_response is a book object" do
       expect(json_response.keys).to match_array(book_keys)
+    end
+
+    it "checks if created values equal params values" do
       expect(json_response[:title]).to match(book_params[:title])
       expect(json_response[:author]).to match(book_params[:author])
       expect(json_response[:publisher]).to match(book_params[:publisher])
       expect(json_response[:genre]).to match(book_params[:genre])
-      
-
     end
   end
 
   describe "PUT /books/1 #update" do
-    it "returns the updated book" do
+    
+    update_params = {:title => "Ruby On Rails"}
+
+    before do
       FactoryBot.create(:book)
-
-      update_params = {:title => "Ruby On Rails"}
-      
       put '/api/v1/books/1', params: {book: update_params}
-
+    end
+    
+    it "returns 200 if updated book" do
       expect(response).to have_http_status(200)
+    end
 
+    it "checks if json_response is a book object" do
       expect(json_response.keys).to match_array(book_keys)
-      expect(json_response[:title]).to eql('Ruby On Rails')
+    end
 
+    it "checks if updated values equal params values" do
+      expect(json_response[:title]).to eql('Ruby On Rails')
     end
   end
 
   describe "DELETE /books/1 #destroy" do
-    it "returns the message after deleting the book" do
+    
+    before do
       FactoryBot.create(:book)
-      
       delete '/api/v1/books/1'
+    end
 
+    it "returns 200 if deleted book" do
       expect(response).to have_http_status(200)
+    end
 
+    it "checks if response has message after deleting the book" do
       expect(json_response.keys).to match_array(:message) 
+    end
+
+    it "checks if message received equals expected" do
       expect(json_response[:message]).to eql('Book destroyed with success!')
-      
     end
   end
 
