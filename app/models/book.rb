b8fdebe04aeb51a4cc99acc4cfbaa70f6c804f97
Carrasco <fernando.carrasco0@gmail.com>
2020-01-19 10:43:07 -0300

    # changed some routes names for more intuitive routes.+ trying do document requests with rswag

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   validates :author, presence: true, length: { minimum: 5 }
   validates :publisher, presence: true
-  validates :genre, presence: true, length: { minimum: 5 }
+  validates :genre, presence: true, length: { minimum: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   
+  # Swagger
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  
   namespace :api do
     namespace :v1 do
       resources :books
 
-      get '/books/order/:sort', to: 'books#order_by'
-      get '/:type', to: 'books#get_type_names'
-      get '/books_by/:type/:name', to: 'books#books_by'
+      get '/books/orderBy/:sort', to: 'books#order_by'
+      get 'books/groupBy/:type', to: 'books#get_type_names'
+      get '/books/groupBy/:type/:name', to: 'books#books_by'
     end
   end
 
