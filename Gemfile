e8f8aa68e03ba91b3ec99498cd697b42cb5c3726
Carrasco <fernando.carrasco0@gmail.com>
2020-01-19 14:16:25 -0300

    - removing gem 'rswag'

--- Gemfile
+++ Gemfile
@@ -48,5 +48,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'json'
-
-gem 'rswag'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   
-  # Swagger
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  
   namespace :api do
     namespace :v1 do
       resources :books
